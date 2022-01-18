7b40329781939ff9d933f2e6f803f7443c0ca2ca
Tarik <tarikibrahimovic2016@gmail.com>
2022-01-18 19:21:27 +0100

    array with loops

--- array/app.js
+++ array/app.js
@@ -48,12 +48,12 @@
 // niz2 = [100,2,1,10];
 // console.log(a(niz1,niz2));
 
-let arr1 = [1,2,3];
-let arr2 = [2,30,1];
+// let arr1 = [1,2,3];
+// let arr2 = [2,30,1];
 
-res = [...new Set([...arr1, ...arr2])]
+// res = [...new Set([...arr1, ...arr2])]
 
-arr3 = arr2 + arr1
+// arr3 = arr2 + arr1
 // console.log(res);
 
 // for(let i = 0; i<arr1.length;i++){
@@ -63,4 +63,53 @@ arr3 = arr2 + arr1
 //         }
 //     }
 // }        ne radi resenje
-console.log(arr3); 
+// console.log(arr3); 
+// arr1.splice(3,0,"nesto")
+// console.log(arr1);
+
+
+// let niz = [10, 20, 30, 50, 100];
+
+// idBr = Number(prompt("Unesite indeks broja koji zelite da pomerite: "))
+
+// pomeraj = Number(prompt("Unesite indeks mesta gde zelite da ga stavite: "))
+
+// pom = niz[pomeraj]
+
+// niz.splice(pomeraj,1,niz[idBr])
+
+// niz.splice(idBr, 1, pom)
+
+// console.log(niz);
+
+// console.log(niz[-1]);
+
+// let matrica = [["a",1,true],["b",2,false]]
+// let matrica2 = [[],[],[]]
+// for(let i = 0; i < matrica.length; i++){
+//     for(let j = 0; j < matrica[i].length; j++){
+//         if(typeof matrica[i][j] === "string"){
+//             matrica2[0].push(matrica[i][j])
+//         }
+//         else if(typeof matrica[i][j] === "number"){
+//             matrica2[1].push(matrica[i][j])
+//         }
+//         else if(typeof matrica[i][j] === "boolean"){
+//             matrica2[2].push(matrica[i][j])
+//         }
+//     }
+// }
+
+// console.log(matrica2);
+
+// let niz = [58, "", "abcd", true, false, null, 0];
+
+// console.log(niz.filter((e) => {
+//     return e && e;
+// }))
+
+// console.log(3 && 6);
+
+niz = ["b", 6, "a", "q", 7, 2]
+
+console.log(niz.sort());
