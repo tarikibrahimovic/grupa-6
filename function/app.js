8d5b967669a03eb7ed124e8113aeba04da89f515
Tarik <tarikibrahimovic2016@gmail.com>
2022-01-27 19:01:34 +0100

    functions

--- function/app.js
+++ function/app.js
@@ -21,41 +21,120 @@
 // unos = prompt("Unesite vase ime: ");
 // nekaF(unos);
 
-function bmi(weight,height){
-    let bmi = 0
-    bmi = weight/(height*height)
-    if(bmi <= 18.5)
-    return "Underweight";
-    else if(bmi <= 25.0 && bmi >= 18.5)
-    return "Normal";
-    else if(bmi <= 30.0 && bmi >= 25.0)
-    return "Overweight";
-    else
-    return "Obese";
-}
+// function bmi(weight,height){
+//     let bmi = 0
+//     bmi = weight/(height*height)
+//     if(bmi <= 18.5)
+//     return "Underweight";
+//     else if(bmi <= 25.0 && bmi >= 18.5)
+//     return "Normal";
+//     else if(bmi <= 30.0 && bmi >= 25.0)
+//     return "Overweight";
+//     else
+//     return "Obese";
+// }
 
-console.log(bmi(74,1.85));
+// console.log(bmi(74,1.85));
 
+// function boolToWord(bool){return bool ? "yes" : "no"}
 
+// console.log(boolToWord(false));
 
-function boolToWord(bool){return bool ? "yes" : "no"}
- 
-console.log(boolToWord(false));
+// function makeUpperCase(str){return str.toUpperCase()}
 
-function makeUpperCase(str){return str.toUpperCase()}
+// console.log(makeUpperCase("tarik"));
 
-console.log(makeUpperCase("tarik"));
+// function even_of_odd(num){return num%2===0 ? "even" : "odd" }
 
-function even_of_odd(num){return num%2===0 ? "even" : "odd" }
+// console.log(even_of_odd(2));
 
-console.log(even_of_odd(2));
+// function between(a,b){
+//     let niz = []
+//     for (let i = a; i <= b; i++) {
+//         niz.push(i);
+//     }
+//     return niz
+// }
 
-function between(a,b){
-    let niz = []
-    for (let i = a; i <= b; i++) {
-        niz.push(i);
+// console.log(between(2,10));
+
+// let otac = 45;
+// let sin = 20;
+
+// function twiceAsOld(dadYearsOld,sonYearsOld){
+//    let pom =  dadYearsOld - sonYearsOld * 2;
+//    let datum = new Date()
+//    datum = datum.getFullYear()
+//    if(pom > 0){
+//        return datum += Math.abs(pom)
+//    }
+//    else{
+//        return datum -= pom
+//    }
+// }
+// console.log(twiceAsOld(otac,sin));
+
+// let niz = [1,4,5,6,7,8,9,2];
+
+// function nadji (niz){
+
+//         let i
+//         let key
+//         let j;
+//     for (i = 1; i < niz.length; i++)
+//     {
+//         key = niz[i];
+//         j = i - 1;
+
+//         {
+//             niz[j + 1] = niz[j];
+//             j = j - 1;
+//         }
+//         niz[j + 1] = key;
+//     }
+//         return niz;
+//     }
+
+// let pom = nadji(niz);
+
+// console.log(pom[0]);
+// console.log(pom[1]);
+
+// let niz = [2,3,10,-135235,23523];
+
+// function element(niz){
+//     let max = 0;
+//     for(let i = 0; i < niz.lenght; i++){
+//         if(max<Math.abs(niz[i])){
+//             // max = Math.abs(niz[i]);
+//         }
+//         console.log(Math.abs(niz[i]));
+//     }
+//     // return max;
+// }
+
+// // console.log(element(niz));
+// element(niz)
+
+let niz = [3, 0, 24, 54, 0, 123, 0, 32, 63];
+function nesto(niz) {
+  for (let i = 0; i < niz.length; i++) {
+    if (niz[i] === 0) {
+      niz.splice(i, 1);
+      niz.push(0);
     }
-    return niz
+  }
+  return niz;
 }
 
-console.log(between(2,10));
+console.log(nesto(niz));
+
+function avg(){
+    let suma = 0
+    for (let i = 0; i < niz.length; i++) {
+        suma+=niz[i];        
+    }
+    return suma/niz.length;
+}
+
+console.log(avg(niz));
