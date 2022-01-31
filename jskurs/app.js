52c6647bc676f919ab1f1525084e62c16e4a70c2
Tarik <tarikibrahimovic2016@gmail.com>
2022-01-31 20:32:27 +0100

    exercise

---
+++ jskurs/app.js
@@ -0,0 +1,64 @@
+// let a = window.prompt("Unesite broj :");
+
+// if (a < 0) {
+//   console.log("Broj je manji od nule");
+// } else if (a > 0) {
+//   console.log("Broj je veci od nule");
+// } else {
+//   console.log("Broj je jednak nuli");
+// }
+
+// let b = window.prompt("Unesite niz:");
+// let k = b.split(" ").map(Number);
+// console.log(b.split(" "))
+// console.log(k.sort());
+
+// let broj = window.prompt("Unesite broj");
+// broj = parseInt(broj)
+// switch (broj) {
+//   case 1:
+//     console.log("Januar je");
+//     break;
+//   case 2:
+//     console.log("Februar je");
+//     break;
+//   case 3:
+//     console.log("Mart je");
+//     break;
+//   case 4:
+//     console.log("April je");
+//     break;
+//   case 5:
+//     console.log("Maj je");
+//     break;
+//   case 6:
+//     console.log("Jun je");
+//     break;
+//   case 7:
+//     console.log("Jul je");
+//     break;
+//   case 8:
+//     console.log("Avgust je");
+//     break;
+//   case 9:
+//     console.log("Septembar je");
+//     break;
+//   case 10:
+//     console.log("Oktobar je");
+//     break;
+//   case 11:
+//     console.log("Novembar je");
+//     break;
+//   case 12:
+//     console.log("Decembar je");
+//     break;
+//   default:
+//     console.log("Neisprevan broj");
+// }
+
+
+let niz = [-5, -2, -6, 0, -1]
+
+
+
+console.log(Math.min(...niz));
