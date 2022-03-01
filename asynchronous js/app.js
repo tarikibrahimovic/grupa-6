719f96fcdd8a6b8c2e50594dc6fb4d467633188f
Tarik <tarikibrahimovic2016@gmail.com>
2022-03-01 19:09:04 +0100

    promise exercise

--- asynchronous js/app.js
+++ asynchronous js/app.js
@@ -49,30 +49,78 @@
 
 // console.log(findMissing([1, 2, 3], [1, 3]));
 
-const heros = [
-  {
-    name: "Spider-man",
-  },
-  {
-    name: "Thor",
-  },
-  {
-    name: "Black Panther",
-  },
-  {
-    name: "Capetan Marvel",
-  },
-  {
-    name: "silver Surfer",
-  },
-];
+// const heros = [
+//   {
+//     name: "Spider-man",
+//   },
+//   {
+//     name: "Thor",
+//   },
+//   {
+//     name: "Black Panther",
+//   },
+//   {
+//     name: "Capetan Marvel",
+//   },
+//   {
+//     name: "silver Surfer",
+//   },
+// ];
 
+// let heros2 = heros.map((el, index) => {
+//   return {
+//     id: index,
+//     heros: el.name,
+//   };
+// });
 
-let heros2 = heros.map((el,index) => {
-    return{
-        id:index,
-        heros:el.name
-    }
-})
+// console.log(heros2);
 
-console.log(heros2);
+// let myPromise = new Promise(function (myResolve, myReject) {
+//   let x = 0;
+//   myResolve("OK");
+// });
+
+// myPromise
+//   .then((value) => {
+//     console.log("then: ", value);
+//   })
+//   .catch((value) => {
+//     console.log("catch: ", value);
+//   })
+//   .finally(() => {
+//     console.log("Uvek ce da se izvrsi!!!");
+//   });
+
+// setTimeout(function job(){
+//   let obecanje = new Promise(function (resolve,reject) {
+//     resolve()
+//   });
+//   obecanje.then(() => {
+//     console.log("Hello world");
+//   });
+// },2000);
+
+// function job(){
+//   let obecanje = new Promise(function(resolve,reject) {
+//     setTimeout(resolve(),2000);
+//   })
+
+//   obecanje.then(() => {
+//         console.log("Hello world");
+//       });
+// }
+
+function job() {
+  return "Hello world";
+}
+
+let obecanje = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    resolve(job())},
+   2000);
+});
+
+obecanje.then((value) => {
+  console.log(value);
+});
